5f95f3028d67013dc7e1e8da006b94fbe5863fa2
Asshall <predateo@gmail.com>
2022-02-20 12:03:48 +0100

    Big ol commit with many different shit in it cause I'm lazy
    - Created a grammar, with rule for the following tokens:
    		* Dx
    		* CPU
    		* GPU
    		* Scripts
    		* Fatal
    		* Build
    		* Warnings
    - Created a "system" to process contextual tokens
    - Plug the backend back in
    > Still a rough first pass... Much refactoring to come

--- src/lexers/crashlogs.js
+++ src/lexers/crashlogs.js
@@ -1,12 +1,18 @@
 const moo = require("moo")
 const fs = require("fs")
-
+const vScripts = require("./vanillaScripts.js")
 const errPre = "^! ?";
 const repIden = " \\[\\d+\\]" // For repeated message, in the logs they appear like "msg [number]"
 const nl = "\r\n"; // Windows carriage return before newline... Cause windows is "different"
 const traceIden = "~ -{72}"
-// c:\path!/aha-you_mad.script or ... script.name
-const path = "(?:[a-z]:\\/|\\\.{3} ?)(?:[\\d\\w\\.!-]+[\\/\\\\]?)+" /* Fucken windows path */
+// c:\path!/aha-you_mad.script or ... script.name or ...script.name or ..\script.name
+const path = "(?:[a-z]:|\\\.{2,3} ?)\\\\?(?:[\\d\\w\.!-]+[\\/\\\\]?)+" /* Fucken windows path */
+function baseName(p){
+  if (p.match(path)){
+	// Uses the path regex execpt it makes the last \ or / mandatory so you get the basename
+	return p.replace(new RegExp(path.substr(0,path.length-3) + ")+"), '')
+  } else { return p }
+}
 const linenum = {match: /(?:\(line:|[:\(]) ?\d+[\):]/, value: x => x.match(/\d+/)[0]}
 // This is so that errors are individual lines
 const errorToken = {
@@ -17,7 +23,7 @@ const errorToken = {
 let mainState = {
   CPU: {
 	match: /\* Detected CPU.*/,
-	value: x => x.split(/:|APU|CPU|Processor/)[2],
+	value: x => x.split(/:|APU|CPU|Processor/)[2].trim(),
 	lineBreaks: true
   },
   GPU: {
@@ -57,7 +63,7 @@ let mainState = {
 	push: "trace"
   },
   Fatal: {
-	match: new RegExp(`FATAL ERROR${nl}+`),
+	match: new RegExp(`FATAL ERROR+`),
 	push: "fatal"
   },
   Newline: {match: nl, linebreaks: true},
@@ -67,10 +73,10 @@ lexer = moo.states({
   main: mainState,
   lua: {
 	WS: " ",
-	Ignore: "SCRIPT RUNTIME ERROR",
+	StartMarker: "SCRIPT RUNTIME ERROR",
 	Class: /[A-Z][\d\w]+/,
 	Level: /\d : \[[\w ]+\]/,
-	Path: new RegExp(path),
+	Path: {match: new RegExp(path), value: baseName},
 	Linenum: linenum,
 	Method: {match: /:{2}[\d\w]+/, value: x => x.slice(1)},
 	Sep: ":",
@@ -86,23 +92,56 @@ lexer = moo.states({
   trace: {
 	WS: {match: /\s/, lineBreaks: true},
 	Newline: {match: nl, lineBreaks: true},
-	Path: {match: new RegExp(path), },//value: x => x.replace('... ','')},
+	Path: {match: new RegExp(path),value: baseName},
 	Function: {
 	  match: new RegExp(`['<]?(?:[\\d\\w]+|${path})(?::\\d+)?['>]`),
 	  value: x => x.substr(1, x.length-2)
 	},
 	Linenum: linenum,
 	Ignore: "in function",
-	TraceDelim: {match: new RegExp(traceIden), pop: 1},
-	LexError: errorToken
+	EOT: {match: new RegExp(traceIden), pop: 1},
+	LexError: errorToken,
   },
   fatal: {
-	ErrIden: /\[error\]/,
+	Ignore: {match: new RegExp(`\\[error\\]|${nl}| `), lineBreaks: true},
 	Name: {match: /\w+ +: /, value: x => x.split(' ')[0]},
-	EOF: {match: new RegExp(` (?:${nl}){2}stack trace:(?:${nl}){2}`), pop:1},
-	Newline: {match: nl, lineBreaks: true},
-	Value: /.+/,
+	EOF: {match: /stack trace:/, pop:1},
+	Value: {match: /.+/, value: baseName},
+	LexError: errorToken,
   }
 })
 
-module.exports = lexer
+function itrUntil(lexer, token, skip){
+  let next = lexer.next()
+  let ret = []
+  let ignores = ["LexError", "Newline", "Ignore", skip]
+  while (next.type !== token || next === undefined) {
+	if (ignores.includes(next.type)){next = lexer.next();continue}
+	ret.push(next)
+	next = lexer.next()
+  }
+  return ret
+}
+
+let grammar = {
+  "CPU" : {"default": "No CPU info"},
+  "GPU" : {"default": "No GPU info"},
+  "Build" : {"default": "No build info"},
+  "Dx" : {"default": "No DirectX info"},
+  "Warning" : {"default": "No Warnings"},
+  "Script" : {"precond" : vScripts.includes, "default": "No non-vanilla scripts"},
+  "Fatal" : {"default": "No fatal error", "functor": l => {
+	  let bufferFatal = itrUntil(l, "EOF")
+	  let fatal = []
+	  for (let i=1; i < bufferFatal.length; i+=2){
+		fatal[bufferFatal[i-1].value] = bufferFatal[i].value
+	  }
+	  if (fatal.Arguments !== undefined && fatal.Arguments.split(":")[0] == "LUA error") {
+		return baseName(fatal.Arguments.split(":")[1])
+	  }
+	  return `${fatal.Function} : ${fatal.Expression} in ${fatal.File} (${fatal.Line})\n${fatal.Description}`
+	}
+  },
+}
+
+module.exports = { lexer, grammar }
--- src/logtypes/crash.js
+++ src/logtypes/crash.js
@@ -2,30 +2,38 @@ const Discord = require("discord.js");
 const JSP = require("jspaste");
 const fatalPaste = require("../pastes/fatal.js");
 const nconf = require("nconf");
-let lexer = require("../lexers/crashlogs.js")
-const vScripts = require("../lexers/vanillaScripts.js")
+let { lexer, grammar } = require("../lexers/crashlogs.js")
 const vanillaBuild = "build 8028";
 
 module.exports = async (client, text, message) => {
   let data = parseCrashlog(text);
   //var possiblefix = possiblefixes(data, text);
-
-  const pasteResponse = await JSP.publish(fatalPaste(data));
+  let defaults = {
+	"CPU" : ["No CPU info"],
+	"GPU" : ["No GPU info"],
+	"Build": ["No build info"],
+	"Dx": ["No DirectX info"],
+	"Fatal": ["No fatal error"]
+  }
+  for (let k of Object.keys(defaults)){
+	data[k] = data[k] === undefined ? defaults[k] : data[k]
+  }
+  // const pasteResponse = await JSP.publish(fatalPaste(data));
   const embed = new Discord.MessageEmbed();
   embed.setTitle("Crash report");
   //embed.addField("Description: ", data.description, true);
-  embed.addField("Hardware: ",`CPU: ${data.systemDetails.cpu}\nGPU: ${data.systemDetails.gpu}`, true);
-  embed.addField("Build type: ",data.build, true);
-  embed.addField("DirectX version: ",data.dx, true);
-  embed.addField("Fatal error: ",data.fatal, false);
-  embed.addField("For further investigating: ",pasteResponse.url, false);
+  embed.addField("Hardware: ",`CPU: ${data.CPU[data.CPU.length-1]}\nGPU: ${data.GPU[data.GPU.length-1]}`, true);
+  embed.addField("Build type: ",data.Build[data.Build.length-1], true);
+  embed.addField("DirectX version: ",data.Dx[data.Dx.length-1], true);
+  embed.addField("Fatal error: ",data.Fatal[data.Fatal.length-1], false);
+  // embed.addField("For further investigating: ",pasteResponse.url, false);
   //embed.addField("Possible fix: ", possiblefix, false);
   embed.setTimestamp();
   message.reply({ content: nconf.get("crash-reply"), embeds: [embed]});
 }
 
 function addUnique(arr, el){
-	el = el.replace(tokens.REP) // Removing the repeteat patern
+	if (arr === undefined) { arr = [] }
 	if (!arr.includes(el)) {
 		arr.push(el)
 	}
@@ -34,27 +42,22 @@ function addUnique(arr, el){
 
 function parseCrashlog(crashlog) {
 
-  const lines = crashlog.split("\n");
-	if (lines.length < 1 ) {
-	  throw new Error("Invalid crashlog");
-  }
-
-  let ret = {
-		systemDetails: {},
-		build: "Unknown",
-		dx: "Unknown",
-		scripts: [],
-		tracebacks: [],
-		luaErrors: [],
-		errors: [],
-		warnings: [],
-		fatal: "",
-	};
+  let ret = {};
 
   lexer.reset(crashlog);
-  // Bot's not working but this is debug
   for (let token of lexer) {
-	if (!["LexError", "WS", "Newlines", "Ignore"].includes(token.type)) {console.log(token)}
+	let rule = grammar[token.type]
+	if (rule === undefined){continue}
+	let check = 'precond' in rule ? rule.precond() : true
+	if (check) {
+		let val = 'functor' in rule ? rule.functor(lexer) : token.value === undefined ? rule.default : token.value
+	  ret[token.type] = addUnique(ret[token.type], val)
+	}
   }
+  // Bot's not working but this is debug
+  // for (let token of lexer) {
+  //   if (!["LexError", "WS", "Newline", "Ignore"].includes(token.type)) {console.log(token.type + " : " + token.value)}
+  // }
+  console.log(ret)
   return ret;
 }
